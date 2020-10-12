5997b4510081db63e696bc33fbafab483033b7cd
Peter Steele <steele.peter.3@gmail.com>
2020-10-12 06:19:12 -0400

    initial changes

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, useWindowDimensions } from 'react-native';
 
 export default function App() {
+  const height = useWindowDimensions().height;
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <TouchableOpacity onPress={()=>console.log(height)} style={{backgroundColor:'#ccffcc', padding: 20}}>
+        <Text>Log Height</Text>
+      </TouchableOpacity>
     </View>
   );
 }
